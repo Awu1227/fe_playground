ed8e16f6c5f7a61435de234a4d776ef10cdfbdd7
orange <2497436726@qq.com>
2023-05-16 06:58:08 +0800

    :wrench:更改配置

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  ignoreDeadLinks: true,
   base: "/fe_playground/",
   title: "前端游乐场",
   description: "前端知识点汇集,构建属于自己的知识体系",
